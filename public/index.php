b21eedd92eea418f7f87683ca6d04a87418a14fc
YusufByn <yusuf.buyukaydin33@gmail.com>
2025-07-31 18:49:02 +0200

    second-commit

--- public/index.php
+++ public/index.php
@@ -18,29 +18,48 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 // je créer un array vide, en le rappelant dans mes conditions ca va me permettre d'écrire des phrases directement
     $errorsMessages = []; 
 
-    // if (!empty($formName) && !empty($formFamillyName) && !empty($formAge)) {
-    //     $errorsMessages = []
-    // } 
-// si formName (prénom) est vide, tu m'écris le message d'erreur suivant
-if (empty($formName)) {
-    $errorsMessages[] = "Vous devez nous adresser un prénom.";
+// Si $formName n’est pas vide ET $formFamillyName n’est pas vide ET $formAge n’est pas vide
+// alors tu considères que $errorsMessages est égal à prénom formName etc
+if (!empty($formName) && !empty($formFamillyName) && !empty($formAge)) {
+    $errorsMessages = [
+        "prénom" => $formName,
+        "nom"    => $formFamillyName,
+        "âge"    => $formAge
+    ];
+} else {
+    if (empty($formName)) {
+        // étant donné que dans le tableau assiocatif en haut j'ai associé prénom à formName je dois préciser en bas prénom
+        $errorsMessages["prénom"] = "Le prénom est obligatoire.";
+    }
+    if (empty($formFamillyName)) {
+        // étant donné que dans le tableau assiocatif en haut j'ai associé nom à formFamillyName je dois préciser en bas nom
+        $errorsMessages["nom"] = "Le nom de famille est obligatoire.";
+    }
+    if (empty($formAge)) {
+        // étant donné que dans le tableau assiocatif en haut j'ai associé age à formAge je dois préciser en bas age
+        $errorsMessages["âge"] = "L'âge est obligatoire.";
+    }
 }
-// si formfamillyname est vide, tu m'écris le message d'erreur suivant
-if (empty($formFamillyName)) {
-    $errorsMessages[] = "Vous devez nous adresser un nom de famille.";
-}
-// je pourrai ne pas le faire pour l'age et le mettre en required dans le html mais si quand meme
-if (empty($formAge)) {
-    $errorsMessages[] = "Vous devez nous adresser un âge.";
-}
-// si l'age est inférieur à 18, stop, trop jeune
-if ($formAge < 18) {
-    $errorsMessages[] = "Vous êtes trop jeune pour continuer";
-}
-// si il n'y a pas d'erreurs, tu écris cela
-if (empty($errorsMessages)) {
-    echo "Bonjour " . $formName . " nous avons pris en compte vos informations !";
-}
+// // si formName (prénom) est vide, tu m'écris le message d'erreur suivant
+// if (empty($formName)) {
+//     $errorsMessages[] = "Vous devez nous adresser un prénom.";
+// }
+// // si formfamillyname est vide, tu m'écris le message d'erreur suivant
+// if (empty($formFamillyName)) {
+//     $errorsMessages[] = "Vous devez nous adresser un nom de famille.";
+// }
+// // je pourrai ne pas le faire pour l'age et le mettre en required dans le html mais si quand meme
+// if (empty($formAge)) {
+//     $errorsMessages[] = "Vous devez nous adresser un âge.";
+// }
+// // si l'age est inférieur à 18, stop, trop jeune
+// if ($formAge < 18) {
+//     $errorsMessages[] = "Vous êtes trop jeune pour continuer";
+// }
+// // si il n'y a pas d'erreurs, tu écris cela
+// if (empty($errorsMessages)) {
+//     echo "Bonjour " . $formName . " nous avons pris en compte vos informations !";
+// }
 
 // traitement/nettoyage de données c'est SUPER IMPORTANT de faire ca, faut tout le temps le faire
 // si aucune erreurs, tu converties les charateres special cen entité HTML ET tu trim(genre ca enleve les espaces etcetc)
